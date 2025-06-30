5908d24f3affcc0e8819479eb6f0b6aecd18cc71
wphq <crea8ivedev@gmail.com>
2025-06-30 19:27:39 +0530

    config and netlify changed

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -120,9 +120,9 @@ const config: GatsbyConfig = {
         },
         schema: {
           timeout: 600000,
-          perPage: isNetlify ? 10 : 20,
-          requestConcurrency: isNetlify ? 3 : 5,
-          previewRequestConcurrency: isNetlify ? 2 : 3,
+          perPage: isNetlify ? 10 : 10,
+          requestConcurrency: isNetlify ? 3 : 3,
+          previewRequestConcurrency: isNetlify ? 2 : 2,
         },
         type: {
           Category: {
